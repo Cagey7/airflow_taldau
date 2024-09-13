8d9e5a262558ea130faed21bd4fb511b28d8f19b
Cagey7 <96425032+Cagey7@users.noreply.github.com>
2024-09-13 15:30:19 +0500

    new indices

--- dags/load_data.py
+++ dags/load_data.py
@@ -22,7 +22,8 @@ def load_index(index_id):
 with DAG('data_load', default_args=default_args, catchup=False, concurrency=1, max_active_runs=1, schedule_interval=None) as dag:
     index_ids = [701276,702974,702972,702976,704447,704448,704449,704498,19722414,
              2709379,2709380,701188,701592,703831,701625,701227,701228,701925,701939,702022,
-             702041,700917,4023003,4023004,701830,701852,703076,703039,2979005]
+             702041,700917,4023003,4023004,701830,701852,703076,703039,2979005,702740,4244622, 
+             702742,3782164,702944,702943,702840,702835,703857,701608,701850,700925,700916,20558808]
     for index_id in index_ids:
         task_id = f"load_index_id_{index_id}"
         PythonOperator(
@@ -30,3 +31,5 @@ with DAG('data_load', default_args=default_args, catchup=False, concurrency=1, m
             python_callable=load_index,
             op_kwargs={'index_id': index_id}
         )
+
+
